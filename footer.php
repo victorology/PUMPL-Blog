0a945b20c683a6a36fc2223fc77a251d6d28aa5e
Victor Ching <v@victor.is>
2010-11-28 17:27:13 +0900

    made all links live

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <div id="footer">
 	<div class="container">
-		<div id="footer_home"><a href="#">홈</a></div>
+		<div id="footer_home"><a href="http://pumpl.com">홈</a></div>
 			<div class="header_divider"></div>
 		<div id="footer_blog"><a href="http://blog.pumpl.com">블로그</a></div>
 			<div class="header_divider"></div>
@@ -12,8 +12,8 @@
 		<div id="footer_email"><a href="mailto:victor@pumpl.com">victor@pumpl.com</a></div>
 			<div class="header_divider"></div>
 		<div id="footer_subscribe">구독하기:</div>
-		<div id="footer_twitter"><a href="http://twitter.com/pumpl">트위터</a></div>
-		<div id="footer_facebook"><a href="http://facebook.com">페이스북</a></div>
+		<div id="footer_twitter"><a href="http://twitter.com/pumpl" target="_blank">트위터</a></div>
+		<div id="footer_facebook"><a href="http://www.facebook.com/pages/peompeul-PUMPL/104009903004026" target="_blank">페이스북</a></div>
 		<div id="footer_logo"><a href="#">PUMPL</a></div>
 	</div> <!-- .container -->
 </div> <!-- #footer -->
