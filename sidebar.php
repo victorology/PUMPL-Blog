f8e2fdfc3fd495bf6da6560b84b46eae05fc69d8
Victor Ching <v@victor.is>
2010-12-01 16:58:54 +0900

    fixed Twitter box in Firefox

--- sidebar.php
+++ sidebar.php
@@ -6,37 +6,39 @@
 			<script src="http://connect.facebook.net/ko_KR/all.js#xfbml=1"></script><fb:like-box href="http://www.facebook.com/pages/peompeul-PUMPL/104009903004026" width="204" connections="10" stream="false" header="false"></fb:like-box>
 		</div>
 		<div id="twitter_header">PUMPL Twitter</div>
-		<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-		<script>
-		new TWTR.Widget({
-		  version: 2,
-		  type: 'profile',
-		  rpp: 8,
-		  interval: 6000,
-		  width: 204,
-		  height: 248,
-		  theme: {
-		    shell: {
-		      background: '#aecfe4',
-		      color: '#181818'
-		    },
-		    tweets: {
-		      background: '#ffffff',
-		      color: '#181818',
-		      links: '#4f88ac'
-		    }
-		  },
-		  features: {
-		    scrollbar: false,
-		    loop: false,
-		    live: true,
-		    hashtags: true,
-		    timestamp: true,
-		    avatars: false,
-		    behavior: 'all'
-		  }
-		}).render().setUser('PUMPL').start();
-		</script>
+		<div style="clear:both">
+			<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+			<script>
+			new TWTR.Widget({
+			  version: 2,
+			  type: 'profile',
+			  rpp: 8,
+			  interval: 6000,
+			  width: 204,
+			  height: 248,
+			  theme: {
+			    shell: {
+			      background: '#aecfe4',
+			      color: '#181818'
+			    },
+			    tweets: {
+			      background: '#ffffff',
+			      color: '#181818',
+			      links: '#4f88ac'
+			    }
+			  },
+			  features: {
+			    scrollbar: false,
+			    loop: false,
+			    live: true,
+			    hashtags: true,
+			    timestamp: true,
+			    avatars: false,
+			    behavior: 'all'
+			  }
+			}).render().setUser('PUMPL').start();
+			</script>
+		</div>
 	</div> <!-- #sidebar_main -->
 	<div id="sidebar_bot"></div>
 </div><!--sidebar-->
