4007161b16b63414059b1aa408df799b55860b10
Victor Ching <v@victor.is>
2010-12-03 15:06:47 +0900

    fixing tweet button to display count

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 			
 			<div class="share_buttons">
 				<div class="share_twitter">
-					<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title(); ?>" data-count="horizontal" data-via="PUMPL">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+					<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink() ?>" data-text="<?php the_title(); ?>" data-count="horizontal" data-via="PUMPL" data-counturl="<?php the_permalink() ?>" data-related="victorology:펌플의 창업자">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 				</div> <!-- .share_twitter -->
 				<div class="share_facebook">
 					<script src="http://connect.facebook.net/ko_KR/all.js#xfbml=1"></script><fb:like href="<?php the_permalink() ?>" layout="button_count" show_faces="false" width="225" font="arial"></fb:like>
